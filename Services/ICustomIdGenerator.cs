9e02480ba47a24ceb12bc8ca2ab53da71e9cc484
Muntakim <97120249+munta-cream@users.noreply.github.com>
2025-09-09 19:39:04 +0600

    Delete Services directory

--- Services/ICustomIdGenerator.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json;
-
-namespace Inventory_Management_Requirements.Services
-{
-    public interface ICustomIdGenerator
-    {
-        string Generate(int inventoryId, JsonDocument format, int sequence);
-    }
-}
